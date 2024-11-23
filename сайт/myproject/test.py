acb0be0f71763bc22211a5e22a94803fe47231f7
Marl1ng <111419524+Marl1ng@users.noreply.github.com>
2024-11-23 15:49:55 +0300

    Бэкенд
    Пока есть функция переключения между вкладками

---
+++ сайт/myproject/test.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/main")
+def main():
+    return render_template("main.html")
+
+
+@app.route('/<string:profession>')
+def skills(profession):
+    return render_template("skills.html")
+
+
+@app.route('/<string:profession>/<string:skills>')
+def test(profession, skills):
+    return render_template("test.html") 
+
+
+@app.route('/<string:profession>/<string:skills>/answer')
+def answer(profession, skills):
+    return render_template("answer.html")
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
